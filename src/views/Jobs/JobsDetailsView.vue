58d367aba3712c5e59b3541d29c0f606a100d660
hugoresende <hugo.resende@iservices.pt>
2023-10-28 09:35:07 +0100

    json server, fetch data

--- src/views/Jobs/JobsDetailsView.vue
+++ src/views/Jobs/JobsDetailsView.vue
@@ -1,12 +1,33 @@
 <template>
-    <h1>Job details page</h1>
-    <!-- <p>The job id is {{ $route.params.id }}</p> -->
-    <p>The job id is {{ id }}</p>
+    <div v-if="job">
+        <h1>Job details page</h1>
+        <!-- <p>The job id is {{ $route.params.id }}</p> -->
+        <p>The job id is {{ id }}</p>
+        <p> {{ job.title }} </p>
+        <p> {{ job.details }} </p>
+    </div>
+    <div v-else>
+        <p>Loading Job Details</p>
+    </div>
 </template>
 
 <script>
 export default {
     props : ['id'],
+    data() {
+        return {
+            job: null
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            fetch('http://localhost:3000/jobs/' + this.id)
+            .then((res) => res.json())
+            .then((data) => this.job = data)
+            .catch(err => console.log(err.message))
+     
+        }, 1000);
+    }
     //replace by  props: true in index.js
     // data() {
     //     return {
